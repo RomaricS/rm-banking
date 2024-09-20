a2a461b20ebfe8d63601f2983d8e71a277727924
Romaric SEGLA <romaricboss@gmail.com>
2024-09-20 19:55:00 +0200

    Added doughnut component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import HeaderBox from '../../components/headerBox';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const loggedIn = {
+    firstName: 'Jay',
+  };
 
-export default Home
+  return (
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type='greeting'
+            title='Welcome'
+            user={loggedIn.firstName ?? 'Guest'}
+            subtext='Acces and manage your account and transactiosn efficiently'
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1000.85}
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
