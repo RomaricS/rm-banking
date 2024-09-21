1b5d1292705f68e177147e67c684b49fd3cf8e67
Romaric SEGLA <romaricboss@gmail.com>
2024-09-21 16:54:34 +0200

    Added auth

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -14,6 +14,7 @@ import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { AppTitle } from '@/constants';
 import { useRouter } from 'next/navigation';
+import { signIn, signUp } from '@/lib/actions/user.actions';
 
 const AuthForm = ({ type }: { type: string }) => {
     const [user, setUser] = useState(null);
@@ -36,21 +37,23 @@ const AuthForm = ({ type }: { type: string }) => {
     const onSubmit = async (data: z.infer<typeof formSchema>) => {
         setIsLoading(true);
         try {
-            console.log(data);
-
-            if (type === 'sign-in') {
-                // const newUser = await signUp(data);
-                return
+            if (type === 'sign-up') {
+                // SignUp
+                const newUser = await signUp(data);
+                setUser(newUser)
+                return;
             }
 
-            // const response = await signIn({
-            //     email: data.email,
-            //     password: data.password
-            // });
+            // SignIn
+            const { email, password } = data;
+            const response = await signIn({
+                email: email,
+                password: password
+            });
 
-            // if (response) {
-            //     router.push('/')
-            // }
+            if (response) {
+                router.push('/')
+            }
         } catch (error) {
             console.log(error);
         } finally {
